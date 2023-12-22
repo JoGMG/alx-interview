6bff15c244bdb756308cfac8fed08fe1df756211
JoGMG <joattah3@gmail.com>
2023-12-22 05:18:52 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -21,33 +21,48 @@ print these statistics from the beginning:
 import sys
 import signal
 
-total_size = 0
-status_counts = {}
 
-try:
-    for i, line in enumerate(sys.stdin, start=1):
-        parts = line.split()
-        if len(parts) >= 7:
-            status_code = parts[-2]
-            file_size = int(parts[-1])
-
-            # Update total file size
-            total_size += file_size
-
-            # Update status code counts
-            if status_code.isdigit():
-                status_code = int(status_code)
-                status_counts[status_code] = \
-                    status_counts.get(status_code, 0) + 1
-
-            # Print stats after every 10 lines
-            if i % 10 == 0:
-                print(f"File size: {total_size}")
-                for status_code in sorted(status_counts):
-                    print(f"{status_code}: {status_counts[status_code]}")
-except KeyboardInterrupt:
-    pass  # Handle Ctrl+C
-
+def print_stats(total_size, status_counts):
+    """
+    Prints the stats
+    """
     print(f"File size: {total_size}")
     for status_code in sorted(status_counts):
         print(f"{status_code}: {status_counts[status_code]}")
+
+
+def main():
+    """
+    Runs the main operations
+    """
+    total_size = 0
+    status_counts = {}
+
+    try:
+        for i, line in enumerate(sys.stdin, start=1):
+            parts = line.split()
+            if len(parts) >= 7:
+                status_code = parts[-2]
+                file_size = int(parts[-1])
+
+                # Update total file size
+                total_size += file_size
+
+                # Update status code counts
+                if status_code.isdigit():
+                    status_code = int(status_code)
+                    status_counts[status_code] = \
+                        status_counts.get(status_code, 0) + 1
+
+                # Print stats after every 10 lines
+                if i % 10 == 0:
+                    print_stats(total_size, status_counts)
+    except KeyboardInterrupt:
+        pass  # Handle Ctrl+C
+
+    # Print final stats
+    print_stats(total_size, status_counts)
+
+
+if __name__ == "__main__":
+    main()
