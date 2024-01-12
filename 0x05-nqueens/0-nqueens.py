217a652dcdd8ec33eb11652d48bb31415af2a80a
JoGMG <joattah3@gmail.com>
2024-01-12 01:02:12 +0100

    checker

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -61,15 +61,15 @@ def main():
     valid, it prints an error message and exits with status 1.
     """
     if len(sys.argv) != 2:
-        print("Usage: nqueens N\n")
+        print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number\n")
+        print("N must be a number")
         sys.exit(1)
     if n < 4:
-        print("N must be at least 4\n")
+        print("N must be at least 4")
         sys.exit(1)
     solutions = list(solve(n, 0, [], [], []))
     for solution in solutions:
