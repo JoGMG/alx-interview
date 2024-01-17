503b683e8158c3123520e033f12cc116922e70a1
JoGMG <joattah3@gmail.com>
2024-01-18 00:32:14 +0100

    checker

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-/*
-A script that prints all characters of a Star Wars movie:
+// A script that prints all characters of a Star Wars movie:
 
-- The first positional argument passed is the Movie
-  ID - example: `3` = "Return of the Jedi".
-- Display one character name per line in the same order
-  as the "characters" list in the `/films/` endpoint.
-- You must use the `Star wars API`: https://swapi-api.alx-tools.com/
-- You must use the `request` module.
-*/
+// - The first positional argument passed is the Movie
+//   ID - example: `3` = "Return of the Jedi".
+// - Display one character name per line in the same order
+//   as the "characters" list in the `/films/` endpoint.
+// - You must use the `Star wars API`: https://swapi-api.alx-tools.com/
+// - You must use the `request` module.
 const request = require('request');
 const arg = process.argv[2];
 
@@ -24,19 +22,15 @@ function fetchCharacterName(url) {
     });
 }
 
-if (process.argv.length !== 3) {
-    console.log("Usage: node <file_name> <number>");
-} else {
-    request(`https://swapi-api.alx-tools.com/api/films/${arg}`, {json: true}, (error, response, body) => {
-        if (error) {
-            return console.log(error);
-        }
-    
-        const characters = body.characters;
-        const promises = characters.map(fetchCharacterName)
-    
-        Promise.all(promises)
-            .then(names => names.forEach(name => console.log(name)))
-            .catch(error => console.log(error));
-    });
-}
+request(`https://swapi-api.alx-tools.com/api/films/${arg}`, {json: true}, (error, response, body) => {
+    if (error) {
+        return console.log(error);
+    }
+
+    const characters = body.characters;
+    const promises = characters.map(fetchCharacterName)
+
+    Promise.all(promises)
+        .then(names => names.forEach(name => console.log(name)))
+        .catch(error => console.log(error));
+});
